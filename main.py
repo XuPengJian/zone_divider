1fb313006e96c5dcb5b16b13801921dc7d3efefd
xupj <xupj_work@163.com>
2024-08-20 14:45:13 +0800

    代码添加for循环

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
+import os
 from shapely.geometry import Polygon, Point
 
 # 参数输入部分
 project_path = 'example/4'
 # txt_path = 'example/5.txt'
 # cad_dat_path = 'example/001.DAT'
-output_path = 'example/output.txt'
+output_path = 'example/output'
 
 
+def find_files(directory, suffix='.txt'):
+    # 存储所有.txt文件的路径
+    found_files = []
+    # os.walk()遍历目录
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            # 检查文件扩展名是否为suffix
+            if file.endswith(suffix):
+                # 将文件的完整路径添加到列表中
+                found_files.append(os.path.join(root, file).replace('\\', '/'))
+    return found_files
 
 
-def generate_txt(cad_dat_path, txt_path, output_txt_path):
+# 单个文件生成txt的代码
+def generate_txt(dat_path, txt_path, output_txt_path):
     # 打开dat文件，读取里面的点信息
     poly_line = []
-    with open(cad_dat_path, 'r') as f:
+    with open(dat_path, 'r') as f:
         for line in f:
             dat_data = line.strip().split(',')
             longitude = float(dat_data[2])
@@ -52,3 +65,22 @@ def generate_txt(cad_dat_path, txt_path, output_txt_path):
                 f.write(data_list[i])
 
             print('image_name:', txt_data[0])
+
+
+# 分地块的文件
+dat_files = find_files(project_path, '.DAT')
+dat_files.sort()
+# 分镜头的文件，一般为5个镜头
+txt_files = find_files(project_path, '.txt')
+txt_files.sort()
+
+# 先遍历dat文件作为分块依据
+for i, each_dat in enumerate(dat_files):
+    # 定义新建文件夹的名字
+    name_list = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+    output_dir = os.path.join(output_path, name_list[i])
+    os.makedirs(output_dir, exist_ok=True)
+    for j, each_txt in enumerate(txt_files):
+        output_txt = os.path.join(output_dir, f'{j + 1}.txt').replace('\\', '/')
+        print(output_txt)
+        generate_txt(each_dat, each_txt, output_txt)
